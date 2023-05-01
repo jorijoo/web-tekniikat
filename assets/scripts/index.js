7fdee94ab559999fe809583a425752759bfd5150
jorijoo <84845935+jorijoo@users.noreply.github.com>
2023-05-01 23:14:29 +0300

    Add form style

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,27 @@
-// Empty javaScript file
+/* Sivun runko, Jori */
+
+//
+// IMPORT WEBSITE COMPONENTS USING fetch()
+//
+
+// External components listed here will be fetched (imported)
+const loadComponents = ["nav"];
+
+/**
+ * Replaces a tag with /assets/sites/components/tag.html contents
+ * For component import to function, a webserver is required
+ * @param {string} tag  - Tag to be replaced with tag.html contents
+ */
+const componentFetch = async (tag) => {
+    const targetTag = document.querySelector(tag);
+    const tagSrc = `/assets/sites/components/${tag}.html`;
+    
+    const newTag = await fetch(tagSrc)
+        .then(response => response.text());
+
+    targetTag.insertAdjacentHTML('beforebegin', newTag);
+    targetTag.parentNode.removeChild(targetTag);
+}
+
+// After page has parsed, external components will replace placeholders
+for (component of loadComponents) componentFetch(component);
