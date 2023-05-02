acbe969933085efa6751da42d5dbb406be88a353
jorijoo <84845935+jorijoo@users.noreply.github.com>
2023-05-02 16:14:26 +0300

    Finalize secrets website and add functionality for "important" secret

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,7 +8,6 @@
 const loadComponents = ["nav"];
 const submit = document.getElementById('submitSecret');
 
-
 /**
  * Replaces a tag with /assets/sites/components/tag.html contents
  * For component import to function, a webserver is required
@@ -28,49 +27,59 @@ const componentFetch = async (tag) => {
 // After page has parsed, external components will replace placeholders
 for (component of loadComponents) componentFetch(component);
 
+//
+// SECRETS WEBSITE FUNCTIONALITY
+//
+
+/**
+ * Produces nodes (secrets) at the end of the secrets log according to input
+ */
 const addSecrets = () => {
     const secrets = document.getElementById('secrets');
-    const newNode = document.createElement('article');
-    const newNodeClasses = ['row', 'article', 'primary-article']
-    const now = new Date();
+    const secret = document.getElementById('secret').value;
+    let name = document.getElementById('name').value;
+    name = name.replaceAll(' ', '_'); // Website style dictates spaces to be replaced with underscores
+    const important = document.getElementById('important');
 
+    const now = new Date();
     const month = (now.getMonth() < 10) ? '0' + now.getMonth() : now.getMonth();
     const day = (now.getDay() < 10) ? '0' + now.getDay() : now.getDay();
     const hours = (now.getHours() < 10) ? '0' + now.getHours() : now.getHours();
     const minutes = (now.getMinutes() < 10) ? '0' + now.getMinutes() : now.getMinutes();
 
+    // Initialize node data in an array to create nodes with loops
+    const newNode = document.createElement('article');
+    const newNodeClasses = ['row', 'article', 'primary-article']
+    important.checked && newNodeClasses.push('important-secret');
+    for (c of newNodeClasses) {
+        newNode.classList.add(c);
+    }
     const newNodeContents = [
         [
             ['span', `${now.getFullYear()}/${month}/${day}/`],
             ['span', `${hours}${minutes}/`],
-            ['span', document.getElementById('name').value]
+            ['span', name]
         ],
         [
-            ['p', document.getElementById('secret').value]
+            ['p', secret]
         ]
     ]
+
     newNodeContents.forEach((div) => {
-        // console.log(i);
         let tempChild = document.createElement('div');
         tempChild.classList.add('col-12')
+
         div.forEach((elem) => {
             let tempElem, tempTextNode;
 
-            console.log(elem[1]);
             tempElem = document.createElement(elem[0]);
             tempTextNode = document.createTextNode(elem[1]);
             tempElem.appendChild(tempTextNode);
             tempChild.appendChild(tempElem);
         })
         newNode.appendChild(tempChild);
-        secrets.appendChild(newNode);
     }
     )
-    // newNode.appendChild()
-
-    for (c of newNodeClasses) {
-        newNode.classList.add(c);
-    }
 
     secrets.appendChild(newNode);
 }
